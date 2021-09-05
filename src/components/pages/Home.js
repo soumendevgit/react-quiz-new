6dfc2592c2406cb79f3adfb9a1c0bf5ef8658f98
soumendevgit <dev.soumen2021@gmail.com>
2021-09-05 12:08:55 +0530

    development source code

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,9 +1,8 @@
 import React from 'react'
+import Videos from '../Videos'
 
 export default function Home() {
     return (
-        <div>
-            Hello
-        </div>
+        <Videos />
     )
 }
