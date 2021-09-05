cd9e701534992426b1834a0643cda27bfa3da809
soumendevgit <dev.soumen2021@gmail.com>
2021-09-05 11:37:19 +0530

    development source code

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,15 +1,15 @@
 import React from 'react';
-import logo from '../assets/images/logo-bg';
+import logo from '../assets/images/logo-bg.png';
 import classes from '../styles/Nav.module.css';
 import Account from './Account';
 
 
 export default function Nav() {
     return (
-        <nav class={classes.nav}>
+        <nav className={classes.nav}>
           <ul>
             <li>
-              <a href="index.html" class={classes.brand}>
+              <a href="index.html" className={classes.brand}>
                 <img src={logo} alt="Learn with Sumit Logo" />
                 <h3>Learn with Sumit</h3>
               </a>
